4f514291ae0b39609c79f81bac89ec13ed7ea254
dgpluks <danielgomespereira2006@gmail.com>
2025-03-25 23:39:08 +0000

    mensagem

---
+++ java.js
@@ -0,0 +1,6 @@
+alert("Informe o raio!!!");
+        let raio = parseInt(prompt("Informe o raio:"));
+        C=2*3.141592653589793*raio;
+        A=3.141592653589793*raio*raio;
+        V=3/4*3.141592653589793*raio*raio*raio;
+        alert("O comprimento do círculo é " + C + "!!!" + "A área do círculo é " + A + "!!!" + "O volume do círculo é " + V + "!!!");
